bdc7841e0f273675f50c64333d91e5ba1d03e81f
Gerald Bryant <hakeem1106@gmail.com>
2019-03-26 17:22:14 +0000

    the setup

---
+++ index.js
@@ -0,0 +1,20 @@
+newer = () => {
+let a = [];
+let b = [];    
+let i =0;
+
+do{
+
+i++;    
+console.log("This is var "+ i);
+console.log("This is ab "+ ab);
+console.log("This is ba "+ ba);
+
+}while(i < 10);
+
+
+
+
+}
+
+newer();
