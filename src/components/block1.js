2dde1d9bb80a4a73c54ce9bfa4e149d825ce20ed
Gerald Bryant <hakeem1106@gmail.com>
2019-04-06 07:09:48 +0000

    change of plans. Working with react to compartmentalize APIs

---
+++ src/components/block1.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Block1 extends React.Component{
+    render(){
+      return(
+          <div>
+        <section>
+        <button id="request">
+        </button>
+
+        </section>
+          </div>
+      );
+
+
+    }
+    
+
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Block1 from './components/block1.js';
+
+ReactDOM.render(<Block1 />, document.getElementById('root'));
+
+document.getElementById('request').innerHTML="Request";
+
